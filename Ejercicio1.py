3886b0b40d579f48621d1c5dc74233886ba76e51
ImKaneki <josemgarriazo@gmail.com>
2024-08-07 10:02:31 -0500

    Recuperacion

---
+++ Ejercicio1.py
@@ -0,0 +1,11 @@
+def max_custom(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+
+# Ejemplo de uso
+num1 = 20
+num2 = 30
+resultado = max_custom(num1, num2)
+print(f"El número mayor entre {num1} y {num2} es: {resultado}")
